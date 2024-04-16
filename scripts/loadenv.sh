4dde1bf4db08dfb7c66c7a8f5045bd46d31aea13
Shifath Nafis <694029+snafis@users.noreply.github.com>
2024-04-16 09:51:23 +0000

    removing ps1 files

--- scripts/loadenv.sh
+++ scripts/loadenv.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+# @Author: shifath.nafis
+# @Date:   2024-03-28 11:53:06
+# @Last Modified by:   shifath.nafis
+# @Last Modified time: 2024-04-16 09:48:31
  #!/bin/sh
 
 . ./scripts/load_azd_env.sh
